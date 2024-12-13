8e9fc9a421d4d66bbde412bd1e37e4703935937b
mlc-kawabata-shohei <kawabata-shohei@mitsue.co.jp>
2024-12-13 13:38:08 +0900

    minorfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import awsAmplify from 'astro-aws-amplify';
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid', // output: 'server'
-  adapter: awsAmplify()
+  adapter: awsAmplify(),
+  redirects: {
+    '/old-page': '/new-page',
+  }
 });
