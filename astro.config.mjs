d234f317a81440e32bb029615aa9678010c63bdd
mlc-kawabata-shohei <kawabata-shohei@mitsue.co.jp>
2024-12-17 14:05:35 +0900

    minorfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,11 @@ import awsAmplify from 'astro-aws-amplify';
 export default defineConfig({
   output: 'hybrid', // output: 'server'
   adapter: awsAmplify(),
-  build: {
-    format: 'directory',
-  },
-  trailingSlash: 'always',
+  //build: {
+  //  format: 'directory',
+  //},
+  //trailingSlash: 'always',
   redirects: {
     '/old-page': '/new-page',
-    //'/life/index.html': '/life-test/',
   },
 });
--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,10 @@ export function onRequest (context, next) {
     // リクエストからデータをインターセプトします
     // 必要に応じて、`locals`内のプロパティを改変します
     //console.trace("middleare");
-    console.log(context.url);
+    if (context.url.pathname == '/life/') {
+        console.log(context.url);
+        return context.rewrite(new Request("/life/index.html"));
+    }
 
     // Responseか`next()`の結果を返します
     return next();
