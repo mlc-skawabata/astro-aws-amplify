1ff0e635fc7fce103c898aaa0f64d47e4bab584d
mlc-kawabata-shohei <kawabata-shohei@mitsue.co.jp>
2024-12-17 11:43:23 +0900

    minorfix

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ export function onRequest (context, next) {
     // リクエストからデータをインターセプトします
     // 必要に応じて、`locals`内のプロパティを改変します
     //console.trace("middleare");
-    //console.log(context.url);
+    console.log(context.url);
 
     // Responseか`next()`の結果を返します
     return next();
